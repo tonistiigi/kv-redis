6e454357123be4e324d1f313ff0d60a88099a3a0
Tõnis Tiigi <tonistiigi@gmail.com>
2012-12-16 13:23:43 +0200

    Start moving redis client code to second file.

---
+++ redis.js
@@ -0,0 +1,13 @@
+/*
+  Simplified Redis client.
+  Returns readable stream for any Redis command.
+  Data type depends on the command.
+*/
+
+function Redis(port, host, auth) {
+
+}
+
+Redis.prototype.exec = function(args) {
+
+}
